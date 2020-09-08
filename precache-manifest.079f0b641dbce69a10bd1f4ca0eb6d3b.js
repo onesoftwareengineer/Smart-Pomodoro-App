cd21ee663f329f13f42b6600e385c94b5afc86e3
onesoftwareengineer <48726358+onesoftwareengineer@users.noreply.github.com>
2020-09-08 12:29:46 +0300

    Updates

---
+++ precache-manifest.079f0b641dbce69a10bd1f4ca0eb6d3b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a13c88cca60133453dfea40c9f85ebc2",
+    "url": "/Smart-Pomodoro-App/index.html"
+  },
+  {
+    "revision": "14078e8d7e1b5c2fe3a6",
+    "url": "/Smart-Pomodoro-App/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "14078e8d7e1b5c2fe3a6",
+    "url": "/Smart-Pomodoro-App/static/js/2.c1c9aa17.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Smart-Pomodoro-App/static/js/2.c1c9aa17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed6c9523e2f7da8aed88",
+    "url": "/Smart-Pomodoro-App/static/js/main.a906cb1e.chunk.js"
+  },
+  {
+    "revision": "a4bff59064fefc462e78",
+    "url": "/Smart-Pomodoro-App/static/js/runtime-main.49512bfc.js"
+  },
+  {
+    "revision": "6bc3824279ce8a204b4cdfdf7a0c2c73",
+    "url": "/Smart-Pomodoro-App/static/media/school.6bc38242.wav"
+  },
+  {
+    "revision": "4cc18d72fed564ba514058c2561c7168",
+    "url": "/Smart-Pomodoro-App/static/media/sunny.4cc18d72.wav"
+  }
+]);
