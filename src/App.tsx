eb546dea60b28858eb7604051f6eecd34dc51693
Daniel <82083960+danielstefanequilobe@users.noreply.github.com>
2021-04-30 08:19:50 +0300

    added loader on front end

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,6 @@
 import React from 'react'
-import tw from 'twin.macro'
+import tw, { css } from 'twin.macro'
 import logo from './assets/images/logo.png'
-
-/*
-import { Button, Logo } from './components'
-const styles = {
-  // Move long class sets out of jsx to keep it scannable
-  container: ({ hasBackground }: { hasBackground: boolean }) => [
-    tw`flex flex-col items-center justify-center h-screen`,
-    hasBackground && tw`bg-gradient-to-b from-electric to-ribbon`,
-  ],
-}
-*/
 
 const App = () => (
   <>
@@ -54,12 +43,12 @@ const App = () => (
     <div tw="w-full h-screen max-h-96 flex justify-center pt-10 relative"
       style={{ backgroundImage: "linear-gradient(to bottom, rgba(208, 230, 255, 0.9), rgba(223, 238, 255, 0.9)), url('https://i.pinimg.com/originals/3b/7f/57/3b7f574e1e03b7bb3d4c7144763678da.jpg')", backgroundSize: "cover", backgroundRepeat: "no-repeat", backgroundPosition: "center center" }}>
       <div tw="max-w-5xl w-full flex flex-wrap -ml-20">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        <Card i={1} />
+        <Card i={2} />
+        <Card i={3} />
+        <Card i={4} />
+        <Card i={5} />
+        <Card i={6} />
       </div>
       <div tw="absolute right-16 bottom-0 text-xs bg-blue-400 text-white py-2 w-48 text-center">
         12 pomodoro / 3.5 hrs planned
@@ -68,13 +57,26 @@ const App = () => (
 
     {/* icons and start button */}
     <div tw="bg-white w-full flex justify-center items-center relative">
+
       <div tw="max-w-5xl w-full flex pt-2 text-xs">
-        <div>notifications</div>
-        <div tw="ml-4">sound</div>
+        <div tw="text-gray-400 flex items-center mr-2.5">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" tw="fill-current h-3 w-3 mr-1">
+            <path fill-rule="evenodd" d="M18 10c0 3.866-3.582 7-8 7a8.841 8.841 0 01-4.083-.98L2 17l1.338-3.123C2.493 12.767 2 11.434 2 10c0-3.866 3.582-7 8-7s8 3.134 8 7zM7 9H5v2h2V9zm8 0h-2v2h2V9zM9 9h2v2H9V9z" clip-rule="evenodd" />
+          </svg>
+          notifications
+        </div>
+        <div tw="text-gray-200 flex items-center">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" tw="fill-current h-3 w-3 mr-1">
+            <path fill-rule="evenodd" d="M9.383 3.076A1 1 0 0110 4v12a1 1 0 01-1.707.707L4.586 13H2a1 1 0 01-1-1V8a1 1 0 011-1h2.586l3.707-3.707a1 1 0 011.09-.217zM14.657 2.929a1 1 0 011.414 0A9.972 9.972 0 0119 10a9.972 9.972 0 01-2.929 7.071 1 1 0 01-1.414-1.414A7.971 7.971 0 0017 10c0-2.21-.894-4.208-2.343-5.657a1 1 0 010-1.414zm-2.829 2.828a1 1 0 011.415 0A5.983 5.983 0 0115 10a5.984 5.984 0 01-1.757 4.243 1 1 0 01-1.415-1.415A3.984 3.984 0 0013 10a3.983 3.983 0 00-1.172-2.828 1 1 0 010-1.415z" clip-rule="evenodd" />
+          </svg>
+          sound
+        </div>
       </div>
+
       <div tw="absolute right-16 top-0 bg-blue-900 text-white text-xs py-2 w-48 text-center">
         start work
       </div>
+
     </div>
 
     {/* footer */}
@@ -82,12 +84,13 @@ const App = () => (
   </>
 )
 
-const Card = (): JSX.Element => {
+const Card = ({ i }: { i: number }): JSX.Element => {
   const randomTask = tasks[Math.floor(Math.random() * tasks.length)]
   return (
     <div tw="w-44 h-36 ml-10 shadow">
       <div style={{ backgroundImage: `url(${randomTask.photo})`, backgroundSize: "cover", backgroundPosition: "center center" }} tw="h-2/3 w-full" />
       <div tw="bg-blue-400 px-3 text-xs text-white h-1/3 relative flex items-center">
+        {i === 3 && <div css={[linePreloader]}></div>}
         <div tw="w-7 h-7 bg-gray-100 absolute rounded-full text-blue-400 flex items-center justify-center font-semibold -top-3 -left-3">{randomTask.pomodoros}</div>
         {randomTask.description}
       </div>
@@ -96,21 +99,28 @@ const Card = (): JSX.Element => {
 }
 
 export default App
-// "background-image:linear-gradient(rgba(135, 80, 156, 0.9), rgba(135, 80, 156, 0.9)), url(https://img.freepik.com/free-photo/hand-hold-magnifying-glass-orange-light-ray_23911-308.jpg?size=626&ext=jpg)"
-// linear-gradient(176deg, rgba(78,88,106,1) 0%, rgba(172,190,222,1) 100%)
-// linear-gradient(to bottom, rgba(245, 246, 252, 0.52), rgba(117, 19, 93, 0.73)), url('https://i.pinimg.com/originals/3b/7f/57/3b7f574e1e03b7bb3d4c7144763678da.jpg')
 
-/*
- <div css={styles.container({ hasBackground: true })}>
-    <div tw="flex flex-col justify-center h-full gap-y-5">
-      <Button variant="primary">Submit</Button>
-      <Button variant="secondary">Cancel</Button>
-      <Button isSmall>Close</Button>
-    </div>
-    <Logo />
-  </div>
-*/
+const linePreloader = css`
+  width: 100%;
+  height: 1.5px;
+  background: linear-gradient(to right, #1E3A8A, #1E3A8A);
+  background-color: #F3F4F6;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  margin: auto;
+  border-radius: 4px;
+  background-size: 20%;
+  background-repeat: repeat-y;
+  background-position: -20% 0;
+  animation: scroll 1.2s ease-in-out infinite;
 
+  @keyframes scroll {
+    50%{background-size: 80%}
+    100%{background-position: 125% 0%;}
+  }
+`
 
 const tasks = [
   {
