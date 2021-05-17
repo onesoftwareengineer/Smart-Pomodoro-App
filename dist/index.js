f2aee86d05f6bdba59d1717d37d0be0dbc634b2b
Daniel <82083960+danielstefanequilobe@users.noreply.github.com>
2021-05-17 19:25:05 +0300

    Updates

--- dist/index.js
+++ dist/index.js
@@ -3,11 +3,11 @@ import.meta.env = __SNOWPACK_ENV__;
 
 import React from './pkg/react.js';
 import ReactDOM from './pkg/react-dom.js';
-import GlobalStyles from './styles/GlobalStyles.js';
 import App from './App.js';
 import { PomodoroContextWrapper } from './context/pomodorosContext.js';
 import { UserContextWrapper } from './context/userContext.js';
-ReactDOM.render( /*#__PURE__*/React.createElement(React.StrictMode, null, /*#__PURE__*/React.createElement(GlobalStyles, null), /*#__PURE__*/React.createElement(UserContextWrapper, null, /*#__PURE__*/React.createElement(PomodoroContextWrapper, null, /*#__PURE__*/React.createElement(App, null)))), document.getElementById('root')); // Hot Module Replacement (HMR) - Remove this snippet to remove HMR.
+import GlobalStylesComponent from './components/GlobalStylesB.js';
+ReactDOM.render( /*#__PURE__*/React.createElement(React.StrictMode, null, "\\", /*#__PURE__*/React.createElement(GlobalStylesComponent, null), /*#__PURE__*/React.createElement(UserContextWrapper, null, /*#__PURE__*/React.createElement(PomodoroContextWrapper, null, /*#__PURE__*/React.createElement(App, null)))), document.getElementById('root')); // Hot Module Replacement (HMR) - Remove this snippet to remove HMR.
 // Learn more: https://www.snowpack.dev/#hot-module-replacement
 
 if (undefined /* [snowpack] import.meta.hot */ ) {
